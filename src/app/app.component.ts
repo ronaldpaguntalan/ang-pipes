ff36150f8b351eafec434c30c3999082c5dd0aea
Ronald Allan <115074375+ronaldpaguntalan@users.noreply.github.com>
2023-08-31 18:41:34 +0800

    Topic: Angular Pipes
    link: https://www.youtube.com/watch?v=CGLdH5ORX-Y

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ang-pipes';
+  toUppercase = "This message will be uppercased";
+  toLowercase = "This message will be lowercased";
+  count = 345963;
+  dcValue = 87.906534;
+  price = 99.99;
+  today: Date = new Date;
+
+  post : object  = {
+    id: 1,
+    postTitle: "Post 1"
+  }
+
+  postArray : Array<string> = [
+    "Post 1",
+    "Post 2",
+    "Post 3",
+    "Post 4",
+    "Post 5",
+    "Post 6"
+  ]
+
+  userDetails = {
+    name: "User 1",
+    city: "New York",
+    countryCode: "US"
+  }
+
+
+  dummyText: string = "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)."
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import { AppendPipe } from './pipes/append.pipe';
+import { AppendCLIPipe } from './Pipe/append-cli.pipe';
+import { SummaryPipe } from './pipes/summary.pipe';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppendPipe,
+    AppendCLIPipe,
+    SummaryPipe
   ],
   imports: [
     BrowserModule
